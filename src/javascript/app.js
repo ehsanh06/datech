0dc2215acf17fef15dda2885e999a7514ede182c
Ehsan Hussain <ehsanhussain991@gmail.com>
2018-09-13 23:59:25 +0100

    Applied JS to form - ready for PHP validation

---
+++ src/javascript/app.js
@@ -0,0 +1,72 @@
+$(() => {
+    let form,
+        formMessages = $('#form-messages'),
+        formSuccess = false;
+
+    // If viewport is 820px and below, remove desktop .App__main
+    // Simple workaround for bug of Mobile version form
+    if ($(window).width() < 820) {
+        $('body main.desktop').remove();
+        form = $('.mobile__form');
+    } else {
+        $('body main.mobile').remove();
+        form = $('.desktop__form')
+    }
+
+    // Form event listener
+    $(form).on('submit', (e) => {
+        e.preventDefault();
+
+        // Serialize form data.
+        let formData = $(form).serialize();
+
+        // Submit form via AJAX
+        $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData
+            })
+
+            // On successful request
+            .done((response) => {
+                // Form message has the success class
+                $(formMessages)
+                    .removeClass('error')
+                    .addClass('success')
+
+                    // Set message text
+                    .text(response);
+
+                // Empty the form
+                $('#fname', '#lname', '#gdpr', '#email').val('');
+
+                console.log(response);
+
+            })
+            .fail((data) => {
+
+                // Form message has the error class
+                $(formMessages)
+                    .removeClass('success')
+                    .addClass('error');
+
+                // Set message text
+                data.responseText !== '' ? $(formMessages).text(data.responseText) : $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            });
+    });
+
+    $("button.form__submit").on('click', () => {
+        let formDataFilled = $('#fname').val().length > 0 && $('#lname').val().length > 0 && $('#email').val().length > 0 && $('#gdpr').is(":checked");
+        console.log("this: ", $(this));
+        console.log('formDataFilled: ' + formDataFilled)
+        if (formDataFilled == true) {
+            formSuccess = true;
+            setTimeout(() => {
+                if (formSuccess === true) {
+                    console.log('Hooray!');
+                }
+            }, 1500);
+        }
+    });
+
+});
